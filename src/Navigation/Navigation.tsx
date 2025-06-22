01f2300e16f358dfc9b5fe39905efe74a203197c
Sondremi <sondre2004@hotmail.no>
2025-06-22 14:27:40 +0200

    Imports

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
 import { Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import ProfileScreen from '../screens/ProfileScreen';
 import GroupScreen from '../screens/GroupScreen';
 import FriendsScreen from '../screens/FriendsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LogInScreen from '../screens/Login';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -15,7 +16,6 @@ const Stack = createNativeStackNavigator();
 const BeerIcon = require('../../assets/icons/noun-beer-7644526.png')
 const ProfileIcon = require('../../assets/icons/noun-profile-4808974.png')
 const FriendsIcon = require('../../assets/icons/noun-friends-4464219.png')
-
 
 function ProfileStack() {
   return (
@@ -34,32 +34,58 @@ function ProfileStack() {
   );
 }
 
-
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => {
-            if (route.name === 'Groups') {
-              return <Image source={BeerIcon} style={{ width: size, height: size, tintColor: color }} />;
-            }
-            if (route.name === 'Profile') {
-              return <Image source={ProfileIcon} style={{ width: size, height: size, tintColor: color }} />;
-            }
-            if (route.name === 'Friends') {
-              return <Image source={FriendsIcon} style={{ width: size, height: size, tintColor: color }} />;
-            }
-          },
-        })}
-      >
-        <Tab.Screen name="Groups" component={GroupScreen} />
-        <Tab.Screen name="Profile" component={ProfileStack} />
-        <Tab.Screen name="Friends" component={FriendsScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator 
+        initialRouteName='Login'>
+        <Stack.Screen 
+          name="Login" 
+          component={(props: any) => (
+            <LogInScreen
+              {...props}
+              onLoginSuccess={(user: any) => {
+                // Handle successful login here, e.g., navigate or update state
+                props.navigation.navigate('MainTabs');
+              }}
+            />
+          )}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="MainTabs" 
+          component={MainTabNavigator}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+function MainTabNavigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Profile"
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Groups') {
+            return <Image source={BeerIcon} style={{ width: size, height: size, tintColor: color }} />;
+          }
+          if (route.name === 'Profile') {
+            return <Image source={ProfileIcon} style={{ width: size, height: size, tintColor: color }} />;
+          }
+          if (route.name === 'Friends') {
+            return <Image source={FriendsIcon} style={{ width: size, height: size, tintColor: color }} />;
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Groups" component={GroupScreen} />
+      <Tab.Screen name="Profile" component={ProfileStack} />
+      <Tab.Screen name="Friends" component={FriendsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default Navigation;
