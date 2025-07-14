ba3aae075af2a08de692d180fe6a235602ff4b36
Sondremi <sondre2004@hotmail.no>
2025-07-14 17:30:21 +0200

    Update firebase config

--- app/services/firebase/FirebaseConfig.ts
+++ app/services/firebase/FirebaseConfig.ts
@@ -1,21 +1,20 @@
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import 'dotenv/config';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4ZKMK015RlWfD_1xYAsmVIceVv0kSx6A",
-  authDomain: "betabeer-3b7fb.firebaseapp.com",
-  projectId: "betabeer-3b7fb",
-  storageBucket: "betabeer-3b7fb.firebasestorage.app",
-  messagingSenderId: "215706928947",
-  appId: "1:215706928947:web:1f63f8c98d9e0318cde046",
-  measurementId: "G-PY5Y5FJ6GC"
+  apiKey: process.env.FIREBASE_API_KEY!,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.FIREBASE_APP_ID!,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 
-export { analytics, app, auth, firestore };
+export { app, auth, firestore };
